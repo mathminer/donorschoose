3f969d6e1d3f91a2011af333c7b84704eab92445
PedroGasparovsky <fauchard@live.com>
2019-05-21 14:58:33 +0100

    predict percentage feito

--- spark_server.py
+++ spark_server.py
@@ -15,7 +15,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession, SQLContext, types
 from pyspark.sql.types import StructType, IntegerType, StringType
 from pyspark.ml.feature import VectorAssembler
-#from pyspark.mllib.regression import LinearRegressionModel
+from pyspark.sql.functions import isnan
 from pyspark.ml.regression import LinearRegression
 from pyspark.sql.functions import Column
 from pyspark.ml import Pipeline
@@ -107,7 +107,11 @@ schemaTeachers = StructType.fromJson({'fields': [
 
 #--------------PATHS--------------
 
-datasetPath = "../dataset_donations/"
+#LOCAL
+#datasetPath = "../dataset_donations/"
+
+#NA CLOUD
+datasetPath = "gs://dados_donorschoose/"
 donationsPath = datasetPath + "Donations.csv"
 donorsPath = datasetPath + "Donors.csv"
 projectsPath = datasetPath + "Projects.csv"
@@ -115,67 +119,78 @@ resourcesPath = datasetPath + "Resources.csv"
 schoolsPath = datasetPath + "Schools.csv"
 teachersPath = datasetPath + "Teachers.csv"
 
+
+
 #-------------LOADS ---------------
 #o spark eh lazy por isso apenas faz as ops depois de uma accao. para nao estar a definir varias vezes o mesmo defino aqui uma vez
-
+'''
 donations =spark.read.schema(schemaDonations).format("csv").options(header="true").load(donationsPath)
 donors = spark.read.schema(schemaDonors).format("csv").options(header="true").load(donorsPath)
 projects = spark.read.schema(schemaProjects).format("csv").options(header="true").load(projectsPath)
 resources = spark.read.schema(schemaResources).format("csv").options(header="true").load(resourcesPath)
 schools = spark.read.schema(schemaSchools).format("csv").options(header="true").load(schoolsPath)
-teachers = spark.read.schema(schemaTeachers).format("csv").options(header="true").load(teachersPath)
+teachers = spark.read.schema(schemaTeachers).format("csv").options(header="true").load(teachersPath)'''
+
+donations =spark.read.csv(donationsPath)
+donors = spark.read.csv(donorsPath)
+projects = spark.read.csv(projectsPath)
+resources = spark.read.csv(resourcesPath)
+schools = spark.read.csv(schoolsPath)
+teachers = spark.read.csv(teachersPath)
+
+lr_model = 0
+projetos_transformados = None
+X = []
 
 
-def K_means():
-    knr = 2;
-	cols = ["Project_Subject_Category_Tree","Project_Subject_Subcategory_Tree","Project_Grade_Level_Category","Project_Resource_Category"]
-	colsa = []
+'''def K_means():
+  knr = 2
+  cols = ["Project_Subject_Category_Tree","Project_Subject_Subcategory_Tree","Project_Grade_Level_Category","Project_Resource_Category"]
+  colsa = []
+  
+  df = projects.select(cols)
+  
+  df = df.where(df.Project_Subject_Category_Tree.isNotNull())
+  df = df.where(df.Project_Subject_Subcategory_Tree.isNotNull())
+  df = df.where(df.Project_Grade_Level_Category.isNotNull())
+  df = df.where(df.Project_Resource_Category.isNotNull())
+  for i in range(len(cols)):
+  	stringIndexer = StringIndexer(inputCol=cols[i], outputCol=cols[i]+"a")
+    model = stringIndexer.fit(df)
+    df = model.transform(df)
+    colsa.append(cols[i]+"a")
 
-	df = projects.select(cols)
-
-	df = df.where(df.Project_Subject_Category_Tree.isNotNull())
-	df = df.where(df.Project_Subject_Subcategory_Tree.isNotNull())
-	df = df.where(df.Project_Grade_Level_Category.isNotNull())
-	df = df.where(df.Project_Resource_Category.isNotNull())
-
-
-	#df.show()
 	
-	for i in range(len(cols)):
-		stringIndexer = StringIndexer(inputCol=cols[i], outputCol=cols[i]+"a")
-		model = stringIndexer.fit(df)
-		df = model.transform(df)
-		colsa.append(cols[i]+"a")
-
 	
-	
-	for i in range(len(cols)):
-		encoder = OneHotEncoder(inputCol=cols[i]+"a", outputCol=cols[i]+"v")
-		encoded = encoder.transform(df)
+  for i in range(len(cols)):
+	  encoder = OneHotEncoder(inputCol=cols[i]+"a", outputCol=cols[i]+"v")
+	  encoded = encoder.transform(df)
 	
 		
-	assembler = VectorAssembler(
-	inputCols=colsa,
-	outputCol="features")
-	output = assembler.transform(encoded)
+  assembler = VectorAssembler(
+  inputCols=colsa,
+  outputCol="features")
+  output = assembler.transform(encoded)
 
-	output.show()
+  output.show()
 
-	'''
-	# Trains a k-means model.
-	kmeans = KMeans().setK(2).setSeed(1)
-	model = kmeans.fit(output)
-	# Evaluate clustering by computing Silhouette score
-	# print(str(knr) + str(ClusteringEvaluator()));
-	'''
+  # Trains a k-means model.
+  kmeans = KMeans().setK(2).setSeed(1)
+  model = kmeans.fit(output)
+  # Evaluate clustering by computing Silhouette score
+  # print(str(knr) + str(ClusteringEvaluator()));'''
+
 
 
 def regressionModel():
-  projects2 = projects.withColumnRenamed('School_ID','School_ID_Projetos')
+  global projetos_transformados
+  global lr_model
+  global X
 
+  projects2 = projects.withColumnRenamed('School_ID','School_ID_Projetos')
   projetos = projects2.filter((projects2.Project_Current_Status == 'Expired') | (projects2.Project_Current_Status == 'Fully Funded'))\
             .join(schools, projects2.School_ID_Projetos == schools.School_ID, how = 'left')
-             
+
   
   lista_projectos = []
   for row in projetos.head(100):
@@ -199,9 +214,14 @@ def regressionModel():
   projetos = projetos.withColumn('Has_Proj_Need_Stat', projetos.Project_Need_Statement.isNotNull())
   projetos = projetos.withColumn('Has_Proj_Subject_Category', projetos.Project_Subject_Category_Tree.isNotNull())
   projetos = projetos.withColumn('Has_Proj_Resource_Category', projetos.Project_Resource_Category.isNotNull())
+
+  colNames = ['Project_Short_Description', 'Project_Need_Statement', 'Project_Subject_Category_Tree', 'Project_Resource_Category','Project_Type',\
+    'Project_Grade_Level_Category','School_Metro_Type','School_State','School_City','School_District']
+  projetos = projetos.na.fill("none", colNames)
  
   projetos = projetos.withColumn("Percentage_Funded", projetos.Sum_Donations/projetos.Project_Cost * 100)\
-    .select('Teacher_ID',
+    .select('Project_ID',
+       'Teacher_ID',
        'Project_Type',
        'Project_Grade_Level_Category',
        'Project_Cost',
@@ -233,84 +253,35 @@ def regressionModel():
   
   indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(projetos) \
     for column in list(set(projetos.columns)-set(['Project_Cost','School_Percentage_Free_Lunch','Has_Proj_Short_Desc',\
-      'Has_Proj_Need_Stat','Has_Proj_Subject_Category','Has_Proj_Resource_Category'])) ]
+      'Has_Proj_Need_Stat','Has_Proj_Subject_Category','Has_Proj_Resource_Category','Project_ID'])) ]
 
   pipeline = Pipeline(stages=indexers)
-  projetos_transformados = pipeline.fit(projetos).transform(projetos)
+  projetos_transformados = pipeline.fit(projetos).transform(projetos).cache()
 
   vectorAssembler = VectorAssembler(inputCols = X, outputCol = 'features')
   vprojetos_df = vectorAssembler.transform(projetos_transformados)
   vprojetos_df = vprojetos_df.select(['features', 'Percentage_Funded'])
-
-  splits = vprojetos_df.randomSplit([0.7, 0.3])
-  train_df = splits[0]
-  test_df = splits[1]
   
   lr = LinearRegression(featuresCol = 'features', labelCol='Percentage_Funded', maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
-  lr_model = lr.fit(train_df)
-
-  print("Coefficients: " + str(lr_model.coefficients))
-  print("Intercept: " + str(lr_model.intercept))
-  trainingSummary = lr_model.summary
-  print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
-  print("r2: %f" % trainingSummary.r2)
-  train_df.describe().show()
+  lr_model = lr.fit(vprojetos_df)
 
 @app.route('/donorschoose/projects/predictPercentage', methods=['GET'])
 def predict_percentage():
   project = request.args.get('project', None)
   
-  projecto_resultado = projects.filter(projects.Project_ID == project).head(1)
-
-  projects2 = projecto_resultado.withColumnRenamed('School_ID','School_ID_Projetos')
-
-  projetos = projects2.filter((projects2.Project_Current_Status == 'Expired') | (projects2.Project_Current_Status == 'Fully Funded'))\
-            .join(schools, projects2.School_ID_Projetos == schools.School_ID, how = 'left')
-             
+  projecto_resultado = projetos_transformados.filter(projetos_transformados.Project_ID == project)
   
-  lista_projectos = []
-  for row in projetos.head(100):
-    lista_projectos.append(str(row.Project_ID))
+  vectorAssembler = VectorAssembler(inputCols = X, outputCol = 'features')
+  vprojetos_df = vectorAssembler.transform(projecto_resultado)
+  vprojetos_df = vprojetos_df.select(['features', 'Percentage_Funded'])
 
-  doacoes = donations.filter(donations.Project_ID.isin(lista_projectos))\
-    .groupby('Project_ID')\
-    .sum('Donation_Amount').withColumnRenamed("sum(Donation_Amount)", "Sum_Donations")\
-    .withColumnRenamed("Project_ID", "Proj_ID")
+  predictions = lr_model.transform(vprojetos_df)
+  result = predictions.select("prediction","Percentage_Funded",'features').head(1)
 
-  projetos.createOrReplaceTempView('projetos')
-  doacoes.createOrReplaceTempView('doacoes')
-
-  query = ('SELECT * \
-            FROM doacoes, projetos  \
-            WHERE Project_ID = Proj_ID')
-
-  projetos = spark.sql(query)
-
-  projetos = projetos.withColumn('Has_Proj_Short_Desc', projetos.Project_Short_Description.isNotNull())
-  projetos = projetos.withColumn('Has_Proj_Need_Stat', projetos.Project_Need_Statement.isNotNull())
-  projetos = projetos.withColumn('Has_Proj_Subject_Category', projetos.Project_Subject_Category_Tree.isNotNull())
-  projetos = projetos.withColumn('Has_Proj_Resource_Category', projetos.Project_Resource_Category.isNotNull())
- 
-  projetos = projetos.withColumn("Percentage_Funded", projetos.Sum_Donations/projetos.Project_Cost * 100)\
-    .select('Teacher_ID',
-       'Project_Type',
-       'Project_Grade_Level_Category',
-       'Project_Cost',
-       'Has_Proj_Short_Desc',
-       'Has_Proj_Need_Stat',
-       'Has_Proj_Subject_Category',
-       'Has_Proj_Resource_Category',
-       'School_Metro_Type',
-       'School_Percentage_Free_Lunch',
-       'School_State',
-       'School_City',
-       'School_District',
-       'Percentage_Funded')
-
-  #predictions = lr_model.transform()
-  #predictions.select("prediction","MV","features")
   answer = []
+  for row in result:
+       answer.append('Prediction for project ' + project + ':' + str(row.prediction))
   return jsonify(answer)
 
 @app.route('/donorschoose/projects/findByDonor', methods=['GET'])
